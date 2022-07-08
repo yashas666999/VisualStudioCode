9cbeabb33bcad30b0222f3dd85ecdc94006a73b9
yashas666999 <108930241+yashas666999@users.noreply.github.com>
2022-07-08 19:58:57 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+
+console.log("hello");
+console.log("my name is Yashas");
+console.log("i am 12 years old");
+console.log("i live in Brisbane,QLD Australia");
+console.log("my hobbies are: gaming, soccer, drawing,");
+console.log("my favourite type of game is roblox");
+console.log("my favourite sport is soccer I am in a garla team for my school");
